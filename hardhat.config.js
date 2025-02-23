4714cc8923e5edefa95283ceebadcb516afe8798
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-23 09:24:41 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox-viem");
 require("dotenv").config(); // Load environment variables
 
 module.exports = {
   solidity: {
-    version: "0.8.28",  // 🔹 Solidity Version
+    version: "0.8.27",  // 🔹 Solidity Version
     settings: {
       optimizer: {
         enabled: true,
@@ -15,6 +15,7 @@ module.exports = {
   networks: {
     monadTestnet: {
       url: "https://testnet-rpc.monad.xyz", // Monad Testnet RPC
+      chainId: 10143,
       accounts: [process.env.PRIVATE_KEY] // Wallet private key
     }
   },
