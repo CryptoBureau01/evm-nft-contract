8d08d11381b0fc7475ca1b7f22c8fdbeb5794938
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-24 15:35:03 +0530

    Create constructor-args.js

---
+++ scripts/constructor-args.js
@@ -0,0 +1,5 @@
+module.exports = [
+  5000000000, // maxSupply
+  ethers.parseEther("1.0"), // mintPrice
+  "https://gateway.pinata.cloud/ipfs/bafybeig7ckgnpsqefislvfye7gqmoun46w76a355tdxbmmlgms4bl25dsy/" // baseURI
+];
